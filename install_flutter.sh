c2fc053fd9687594fa0d8fb70f442ca56c03226f
jigsaw007 <joshianuj005@gmail.com>
2025-10-22 17:38:43 +0900

    Fix Netlify Flutter install

--- install_flutter.sh
+++ install_flutter.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 set -e
-git clone https://github.com/flutter/flutter.git -b stable
+
+# Install Flutter from GitHub stable channel
+git clone https://github.com/flutter/flutter.git -b stable --depth 1
+
+# Add Flutter to PATH
 export PATH="$PATH:`pwd`/flutter/bin"
+
+# Verify installation
 flutter doctor
+flutter --version
+flutter pub get
