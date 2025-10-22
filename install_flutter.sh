1bfa0cb75c535340d912c2602ca3335e15714cd4
jigsaw007 <joshianuj005@gmail.com>
2025-10-22 17:43:43 +0900

    Fix Netlify Flutter PATH issue

--- install_flutter.sh
+++ install_flutter.sh
@@ -3,16 +3,11 @@ set -e
 
 echo "🚀 Installing Flutter into /opt/buildhome/flutter..."
 
-# Clean any previous Flutter folder (safe)
 rm -rf /opt/buildhome/flutter
-
-# Clone the stable channel
 git clone https://github.com/flutter/flutter.git -b stable /opt/buildhome/flutter
 
-# Add to PATH (for this shell)
 export PATH="/opt/buildhome/flutter/bin:$PATH"
 
-# Verify installation
 flutter --version
 flutter doctor
 flutter pub get
