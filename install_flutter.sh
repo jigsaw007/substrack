7e583abdfc1f4325ee0ce34a91358090fcd1b315
jigsaw007 <joshianuj005@gmail.com>
2025-10-22 18:21:16 +0900

    fix: executable flutter install script

--- install_flutter.sh
+++ install_flutter.sh
@@ -3,17 +3,13 @@ set -e
 
 echo "🚀 Installing Flutter into /opt/buildhome/flutter..."
 
-# Clean and install Flutter SDK
 rm -rf /opt/buildhome/flutter
 git clone https://github.com/flutter/flutter.git -b stable /opt/buildhome/flutter
 
-# Add Flutter to PATH
 export PATH="/opt/buildhome/flutter/bin:$PATH"
 
 flutter --version
 flutter doctor
-
-# Get project dependencies
 flutter pub get
 
 echo "✅ Flutter installed successfully!"
